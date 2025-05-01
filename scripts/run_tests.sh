3363d7f78995c4bf27347233011b08139e8d1356
Daniel Oliveira <do1167475@gmail.com>
2025-05-01 17:29:07 -0300

    Teste para windows e ubuntu.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,8 +3,18 @@
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 ROOT_DIR="$(cd "$BASE_DIR/.." && pwd)"
 
+IS_WINDOWS=false
+case "$(uname -s)" in
+    MINGW*|MSYS*|CYGWIN*)
+        IS_WINDOWS=true
+        ;;
+esac
 "$BASE_DIR/build_runtime.sh"
 
 mkdir $ROOT_DIR/testes/out 
 cd $ROOT_DIR/testes/out && cmake .. && cmake --build .
-cd $ROOT_DIR/testes/out && ./testes
+if [ "$IS_WINDOWS" = true ]; then
+    cd $ROOT_DIR/testes/out && .\testes.exe
+else
+    cd $ROOT_DIR/testes/out && ./testes
+fi
