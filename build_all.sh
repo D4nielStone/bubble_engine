9318f0aa6f778a7a65e8d53f522c1d416911f24c
Daniel Oliveira <do1167475@gmail.com>
2025-05-02 16:33:00 -0300

    Atualização automática

--- build_all.sh
+++ build_all.sh
@@ -3,18 +3,7 @@
 set -e
 
 # Caminho raiz do projeto
-ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
-
-# Caminho para o vcpkg no Windows
-VCPKG_TOOLCHAIN_FILE="$ROOT_DIR/vcpkg/scripts/buildsystems/vcpkg.cmake"
-
-# Detectar Windows (Git Bash) ou Linux
-IS_WINDOWS=false
-case "$(uname -s)" in
-    MINGW*|MSYS*|CYGWIN*)
-        IS_WINDOWS=true
-        ;;
-esac
+ROOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Função para configurar e compilar um diretório
 build_project() {
@@ -25,17 +14,7 @@ build_project() {
     mkdir -p "$OUT_DIR"
     cd "$OUT_DIR"
 
-    if [ "$IS_WINDOWS" = true ]; then
-        # Set GCC as the compiler explicitly
-        cmake .. \
-            -G "MinGW Makefiles" \
-            -DCMAKE_TOOLCHAIN_FILE="$VCPKG_TOOLCHAIN_FILE" \
-            -DCMAKE_BUILD_TYPE=Debug \
-            -DCMAKE_C_COMPILER=gcc \
-            -DCMAKE_CXX_COMPILER=g++    
-    else
-        cmake .. -DCMAKE_BUILD_TYPE=Debug
-    fi
+    cmake .. -DCMAKE_BUILD_TYPE=Debug
 
     cmake --build .
 
