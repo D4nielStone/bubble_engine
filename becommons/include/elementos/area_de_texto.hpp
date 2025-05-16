5a004d5fcefe2b75cfc332c3f8f9cd1d84dcf56b
Daniel Oliveira <d4niel_oliveira@outlook.com>
2025-05-16 16:43:55 -0300

    Atualização automática

--- becommons/include/elementos/area_de_texto.hpp
+++ becommons/include/elementos/area_de_texto.hpp
@@ -39,37 +39,6 @@ namespace BECOMMONS_NS {
          * Exemplo: caixa_de_texto
          */ 
         class area_de_texto : public caixa {
-<<<<<<< HEAD
-            public:
-            // flags de ativação
-            bool m_pode_tocar { false }, m_selecionado { false }, m_mouse_cima { false };
-            
-            tipo_caixa tipo() const override { return tipo_caixa::caixa_de_texto; }
-            virtual bool selecionado() {
-                bool tocou = janela::obterInstancia().m_inputs.isKeyPressed("MouseE");
-                // se não está pressionando, está solto
-                bool esta_solto = !tocou;
-
-                // se está solto e dentro do campo & não está selecionado pode tocar
-                m_pode_tocar = esta_solto && mouseEmCima() && !m_selecionado ? true : false;
-
-                // se tocou fora do campo, deseleciona
-                if(!m_mouse_cima && tocou) m_selecionado = true;
-
-                // se pode tocar e tocou, seleciona
-                if(m_pode_tocar && tocou) m_selecionado = true;
-
-                return m_selecionado;
-            }
-            bool mouseEmCima() const {
-                // vetor2 do mouse
-                dvet2 m = obterMouse();
-                bool m_mouse_cima = (m.x > m_estilo.m_limites.x && m.x < m_estilo.m_limites.z + m_estilo.m_limites.x &&
-                   m.y > m_estilo.m_limites.y && m.y < m_estilo.m_limites.w + m_estilo.m_limites.y);
-                if(m_mouse_cima) {
-                    janela::obterInstancia().defCursor(janela::cursor::i);
-                    return true;
-=======
         public:
             // indica se o elemento está selecionado
             bool m_selecionado { false };
@@ -100,7 +69,6 @@ namespace BECOMMONS_NS {
                 if (justPressed) {
                     // seleciona somente se o clique começar dentro da área
                     m_selecionado = m_mouse_cima;
->>>>>>> 9d483738fb66e2a8a2e501d255de835bc7c83d20
                 }
 
                 // atualiza estado para o próximo frame
