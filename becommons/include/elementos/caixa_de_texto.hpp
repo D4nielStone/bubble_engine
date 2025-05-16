a408da5dc5c78515a66a3e69ec1978b667b1ef8b
Daniel Oliveira <d4niel_oliveira@outlook.com>
2025-05-16 12:25:28 -0300

    mod: caixa_de_texto.hpp

--- becommons/include/elementos/caixa_de_texto.hpp
+++ becommons/include/elementos/caixa_de_texto.hpp
@@ -35,7 +35,7 @@ namespace BECOMMONS_NS {
     namespace elementos {
         class caixa_de_texto : public area_de_texto {
             private:
-                std::string m_buffer {"buffer_test"};
+                std::string m_buffer {""};
                 cor m_cor_antiga, m_acrescimo_cor { 0.1f, 0.1f, 0.25f };
                 float m_borda_antiga, m_acrescimo_borda { 1.f };
                 bool* m_dica_bool;
@@ -43,6 +43,7 @@ namespace BECOMMONS_NS {
                 caixa_de_texto() = default;
                 void configurar() override {
                     m_estilo.m_flag_estilo |= flag_estilo::modular;
+                    m_estilo.m_padding_geral = { 5, 2 };
                     m_borda_antiga = m_estilo.m_espessura_borda;
                     m_cor_antiga = m_estilo.m_cor_borda.b;
                     auto m_texto = adicionarFilho<elementos::texto>(m_id + "_dica", "Digite um nome ...", cor(1, 1, 1, 0.5));
@@ -54,7 +55,7 @@ namespace BECOMMONS_NS {
                 void atualizarInputs() {
                     m_estilo.m_cor_borda = selecionado() ? m_cor_antiga + m_acrescimo_cor : m_cor_antiga;
                     m_estilo.m_espessura_borda = m_selecionado ? m_borda_antiga + m_acrescimo_borda : m_borda_antiga;
-                    *m_dica_bool = m_selecionado;
+                    *m_dica_bool = m_buffer.empty();
                 }
         };
     } 
